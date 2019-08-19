9ea7e357c038b6417d20031f890eccab07522723
seongjin571 <gkstjdwls11@naver.com>
2019-08-19 19:01:42 +0900

    vue router

---
+++ js/app.js
@@ -0,0 +1,16 @@
+Vue.component('todo-footer',{
+    template: '<p>This is another global child component</p>'
+})
+const cmp = {
+    template: '<p>This is another local child component</p>'
+}
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'This is a parent component'
+    },
+    components: {
+        'todo-list': cmp
+    }
+})
